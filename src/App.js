2d1da4bf8ae2c8e0dcd44c16d5c1ed5455e57840
Will Worth <will.worth@gmail.com>
2019-06-03 11:12:27 +0200

    working eg

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Clicker from './Clicker';
+
+function App() {
+  return (
+    <div className="App">
+     <Clicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clicker.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Clicker extends Component {
+    constructor(props){
+        super(props);
+        this.state = {num :1 };
+        this.genRandom = this.genRandom.bind(this);
+    }
+    genRandom(){
+        let rand = Math.floor(Math.random() * 10) + 1;
+        this.setState ({num: rand});
+    }
+    render() {
+        return (
+            <div>
+                <h1>Number is {this.state.num}</h1>
+                <button onClick={this.genRandom} >Random Number</button>
+            </div>
+        )
+    }
+}
